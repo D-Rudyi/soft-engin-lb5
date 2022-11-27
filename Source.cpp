1752fa9b1e82932a783d2a8dda92ba6dbf3928e5
D-Rudyi <thedarkgrundemperor@gmail.com>
2022-11-27 23:49:20 +0200

    Init application

---
+++ Source.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include "cmath"
+#include <fstream>
+
+using namespace std;
+
+ofstream Data("cppstudio.txt");
+string fileinput;
+
+void write(double y) {
+    cout << "y = " << y << endl;
+}
+
+void read(double& a, double& b, double& h, double& n) {
+    
+    int yes = 1, no = 0;
+    cout << "Ввести а: ";
+    cin >> a;
+    cout << "Ввсети b: ";
+    cin >> b;
+    cout << "Ввести h: ";
+    cin >> h;
+    cout << "Ввести n: ";
+    cin >> n;
+    cout << "Ви хочите записати всі дані до файлу? (yes або no)\n";
+    cin >> fileinput;
+    cout << endl;
+
+}
+double sum, y;
+void solve(double a, double b, double h, double n) {
+    if (n <= 0)
+    {
+        cout << "Помилка введених значень";
+    }
+    else
+    {
+        for (double x = a; x <= b; x += h) {
+
+            if (x < 0)
+            {
+                y = 1;
+                for (double i = 1; i < n; i++)
+                {
+                    sum = 0;
+                    for (double j = 1; j <= n; j++)
+                    {
+                        sum += (x - i * i + j) * (x - i * i + j);
+                    }
+                    y *= sum;
+                }
+            }
+            else if (x >= 0)
+            {
+                for (double i = 0; i < n; i++)
+                {
+                    y += (x - 1) / (i + 1);
+                }
+            }
+            write(y);
+            if (fileinput == "yes")
+            {
+                Data << "|x = " << x << "| |" << "n = " << n << "| |" << "y = " << y << "|" << endl;
+            }
+            else {
+
+            }       
+        }
+        Data.close();
+    }
+}
+    
+int main() {
+
+    setlocale(LC_ALL, "UKRAINIAN");
+    double a, n, b, h;  
+    read(a, b, h, n);
+    cout << "==================" << endl;
+    solve(a, b, h, n);
+    cout << "==================" << endl;
+   
+    return 0;
+
+}
---
+++ UnitTest1.cpp
@@ -0,0 +1,150 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "F:\Users\Юра\source\repos\lb 3.2\lb 3.2\Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+
+    TEST_CLASS(solve_Tests) {       //тестування функції solve
+public:
+    TEST_METHOD(solve_get3and4_416returned) {
+        double x = 3;
+        double n = 4;
+        int expected = 4.16;
+
+        int actual = solve(x, n);
+
+        Assert::AreEqual(expected, actual);
+    }
+public:
+    TEST_METHOD(solve_get2and2_15returned) {
+        double x = 2;
+        double n = 2;
+        double expected = 1.5;
+
+        double actual = solve(x, n);
+
+        Assert::AreEqual(expected, actual);
+    }
+public:
+    TEST_METHOD(solve_get6and8_13returned) {
+        double x = 6;
+        double n = 8;
+        int expected = 13;
+
+        int actual = solve(x, n);
+
+        Assert::AreEqual(expected, actual);
+    }
+public:
+    TEST_METHOD(solve_get6and7_13returned) {
+        double x = 6;
+        double n = 7;
+        int expected = 12.9;
+
+        int actual = solve(x, n);
+
+        Assert::AreEqual(expected, actual);
+    }
+public:
+    TEST_METHOD(solve_get7and4_125returned) {
+        double x = 7;
+        double n = 4;
+        int expected = 12.5;
+
+        int actual = solve(x, n);
+
+        Assert::AreEqual(expected, actual);
+    }
+    };
+    TEST_CLASS(checkValidInput_Tests) {
+public:
+    TEST_METHOD(checkValidInput_get3and4_exceptionNotThrown) {  //тестування функції checkValidInput
+        double x = 3;
+        double n = 4;
+
+        try {
+            checkValidInput();
+            Assert::IsTrue(true);
+        }
+        catch (...) {
+            Assert::Fail();
+        }
+    }
+    };
+}
+
+
+/*namespace UnitTest1
+{
+
+    TEST_CLASS(solve_Tests) {
+public:
+    TEST_METHOD(solve_get3and4_416returned) {
+        double x = 3;
+        double n = 4;
+        int expected = 1;
+
+        int actual = solve(x, n);
+
+        Assert::AreEqual(expected, actual);
+    }
+public:
+    TEST_METHOD(solve_get2and2_15returned) {
+        double x = 2;
+        double n = 2;
+        double expected = 1.5;
+
+        double actual = solve(x, n);
+
+        Assert::AreEqual(expected, actual);
+    }
+public:
+    TEST_METHOD(solve_get6and8_13returned) {
+        double x = 6;
+        double n = 8;
+        int expected = -34;
+
+        int actual = solve(x, n);
+
+        Assert::AreEqual(expected, actual);
+    }
+public:
+    TEST_METHOD(solve_get6and7_13returned) {
+        double x = 6;
+        double n = 7;
+        int expected = 12.9;
+
+        int actual = solve(x, n);
+
+        Assert::AreEqual(expected, actual);
+    }
+public:
+    TEST_METHOD(solve_get7and4_125returned) {
+        double x = 7;
+        double n = 4;
+        int expected = 12.5;
+
+        int actual = solve(x, n);
+
+        Assert::AreEqual(expected, actual);
+    }
+    };
+    TEST_CLASS(checkValidInput_Tests) {
+public:
+    TEST_METHOD(checkValidInput_get3and4_exceptionNotThrown) {
+        double x = 3;
+        double n = 4;
+
+        try {
+            checkValidInput();
+            Assert::Fail();
+        }
+        catch (...) {
+            Assert::IsTrue(true);
+        }
+    }
+    };
+}*/
